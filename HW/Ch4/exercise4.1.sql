019df9793c977fc2893e7949d13c0991b460ec2b
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2023-10-31 13:54:47 +0900

    HW#2 : chapter4.5 exercise / exercise4.1 , exercise4.2

---
+++ HW/Ch4/exercise4.1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE list_of_students (
+    Name VARCHAR(20) NOT NULL,
+    student_number INT NOT NULL,
+    year_of_admission INT NOT NULL,
+    major VARCHAR(16) NOT NULL,
+    phone_number INT NOT NULL,
+    address VARCHAR(128) NOT NULL,
+    cumulative_credits INT NOT NULL DEFAULT 0,
+    average_score INT NOT NULL DEFAULT 0.0,
+    enrollment_information TINYINT(1) NOT NULL DEFAULT 1
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ HW/Ch4/exercise4.2.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `users` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(16) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
+    `nickname` VARCHAR(16) NOT NULL,
+    `profile_link` VARCHAR(128) NOT NULL,
+    `profile_message` VARCHAR(128) NOT NULL,
+    `drop_out` TINYINT(1) NOT NULL DEFAULT 0,
+    `membership_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `channels` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(32) NOT NULL,
+    `who_created_channel` INT NOT NULL,
+    `channel_link` VARCHAR(128) NOT NULL,
+    `maximun_capacity` INT NOT NULL,
+    `dorp_out` TINYINT(1) NOT NULL DEFAULT 0,
+    `creation_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`who_created_channel`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `chats` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `chatting_message` TEXT NOT NULL,
+    `who_created_chat` INT NOT NULL,
+    `chatting_channel` INT NOT NULL,
+    `creation_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`who_created_chat`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`chatting_channel`) REFERENCES `channels`(`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `follows` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `follower` INT NOT NULL,
+    `followee` INT NOT NULL,
+    `creation_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`follower`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`follower`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `blocks` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `who_blocking` INT NOT NULL,
+    `who_blocked` INT NOT NULL,
+    `creation_date` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`who_blocking`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`who_blocked`) REFERENCES `users`(`id`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
